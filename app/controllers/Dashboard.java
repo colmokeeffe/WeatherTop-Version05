7fa1435dab6541e0320d38daf25d7f478b9b3a21
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2021-05-22 22:47:17 +0100

    updated dashboard

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -19,14 +19,14 @@ public class Dashboard extends Controller
     List<Station> stations = member.stations;
     Collections.sort(stations, Comparator.comparing(Station::getName,String.CASE_INSENSITIVE_ORDER));
     for(Station station:stations){
-      while(stations.size()!=0){
+      if(station.readings.size()!= 0) {
     station.maxTempVal = StationAnalytics.getMaxTemp(station.readings).temperature;
     station.minTempVal = StationAnalytics.getMinTemp(station.readings).temperature;
     station.maxWindSpeedVal = StationAnalytics.getMaxWindSpeed(station.readings).windSpeed;
     station.minWindSpeedVal = StationAnalytics.getMinWindSpeed(station.readings).windSpeed;
     station.maxPressureVal = StationAnalytics.getMaxPressure(station.readings).pressure;
     station.minPressureVal = StationAnalytics.getMinPressure(station.readings).pressure;
-    render("dashboard.html", stations);
+    render("dashboard.html", member, stations);
   }}}
 
   public static void addStation (String name, float lat, float lng, Date date)
