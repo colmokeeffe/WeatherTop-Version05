046eca745a32958047d137033469686ee62ece9f
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2021-05-22 23:11:41 +0100

    removal of max/min values in dashboard view

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -18,16 +18,9 @@ public class Dashboard extends Controller
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
     Collections.sort(stations, Comparator.comparing(Station::getName,String.CASE_INSENSITIVE_ORDER));
-    for(Station station:stations){
-      if(station.readings.size()!= 0) {
-    station.maxTempVal = StationAnalytics.getMaxTemp(station.readings).temperature;
-    station.minTempVal = StationAnalytics.getMinTemp(station.readings).temperature;
-    station.maxWindSpeedVal = StationAnalytics.getMaxWindSpeed(station.readings).windSpeed;
-    station.minWindSpeedVal = StationAnalytics.getMinWindSpeed(station.readings).windSpeed;
-    station.maxPressureVal = StationAnalytics.getMaxPressure(station.readings).pressure;
-    station.minPressureVal = StationAnalytics.getMinPressure(station.readings).pressure;
+
     render("dashboard.html", member, stations);
-  }}}
+  }
 
   public static void addStation (String name, float lat, float lng, Date date)
   {
