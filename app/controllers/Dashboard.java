c959e9969e1a2d2dd39086f239ea02801e00ddee
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2021-05-23 22:36:52 +0100

    Dashboard station view update

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -17,9 +17,22 @@ public class Dashboard extends Controller
     Logger.info("Rendering Dashboard");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
+    List<Reading>readings=Reading.findAll();
+    Logger.info("why isnt loop working"+member.stations.size());
     Collections.sort(stations, Comparator.comparing(Station::getName,String.CASE_INSENSITIVE_ORDER));
+    for (Station station: stations){
+      if(station.readings.size()!= 0){
+      for (int i = 0; i <= member.stations.size(); i++)
+        Logger.info("why isnt loop working"+member.stations.size());
+      station.maxTempVal = StationAnalytics.getMaxTemp(station.readings).temperature;
+      station.minTempVal = StationAnalytics.getMinTemp(station.readings).temperature;
+      station.maxWindSpeedVal = StationAnalytics.getMaxWindSpeed(station.readings).windSpeed;
+      station.minWindSpeedVal = StationAnalytics.getMinWindSpeed(station.readings).windSpeed;
+      station.maxPressureVal = StationAnalytics.getMaxPressure(station.readings).pressure;
+      station.minPressureVal = StationAnalytics.getMinPressure(station.readings).pressure;
 
-    render("dashboard.html", member, stations);
+    }}
+    render("dashboard.html",stations);
   }
 
   public static void addStation (String name, float lat, float lng, Date date)
